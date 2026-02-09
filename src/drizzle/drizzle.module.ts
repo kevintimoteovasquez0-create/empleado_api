f86caecf2fd3cdef6d3f604f9503daaf98e18575
Brayan-Uwu-Onichan <Panchomancorre90@gmail.com>
2026-02-09 16:51:49 -0500

    importar drizzle y exportar service

--- src/drizzle/drizzle.module.ts
+++ src/drizzle/drizzle.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DrizzleService } from './drizzle.service';
 
+// Se agrega decorador @Global() para que el módulo esté disponible en toda la aplicación
+@Global()
 @Module({
   providers: [DrizzleService],
+  // Se asegura que DrizzleService esté exportado para que otros módulos puedan inyectarlo
   exports: [DrizzleService],
 })
 export class DrizzleModule {}
