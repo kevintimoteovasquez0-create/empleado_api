f0cba5efe3e108abfe937fe7857da3608703c201
kevintimoteovasquez0-create <kevintimoteovasquez0@gmail.com>
2026-03-01 16:07:20 -0500

    cambio fecha ingreso y nacimiento a string

--- src/drizzle/schema/empleado.ts
+++ src/drizzle/schema/empleado.ts
@@ -14,7 +14,7 @@ export const EmpleadoTable = pgTable("empleado",{
   nombres: varchar("nombres", {length: 100}).notNull(),
   apellidos: varchar("apellidos", {length: 100}).notNull(),
   tipo_documento: DocumentoEnum().notNull(),
-  numero_documento: varchar("numero_documento").unique().notNull(),
+  numero_documento: varchar("numero_documento", {length: 12}).unique().notNull(),
   cargo: varchar("cargo", {length: 100}).notNull(),
   tipo_personal: TipoPersonalEnum().notNull(),
   fecha_ingreso: timestamp("fecha_ingreso").notNull(),
